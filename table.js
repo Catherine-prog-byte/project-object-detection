6b9a7cffdc48d0e2dfcd58402856d9691aabd66b
Catherine-prog-byte <77283675+Catherine-prog-byte@users.noreply.github.com>
2021-12-24 20:01:06 +0530

    Add files via upload

---
+++ table.js
@@ -0,0 +1,38 @@
+img="";
+status_obj="";
+function setup() {
+    canvas= createCanvas(640,420);
+    canvas.center();
+    objectDetector=ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML ="Status : Detecting Objects";
+}
+function preload() {
+    img=loadImage("table.jpg");
+}
+function draw() {
+   image(img,0,0,640,420);
+   fill("#FFB600");
+   text("table",100,100);
+   noFill();
+   textSize(25);
+   stroke("#FFB600");
+   rect(80,80,500,400);
+
+   fill("#FFB600");
+   text("laptop",200,150);
+   noFill();
+   textSize(25);
+   stroke("#FFB600");
+   rect(180,120,150,80);
+}
+function modelLoaded() {
+    console.log("Model Loaded");
+    status_obj=true;
+    objectDetector.detect(img,gotResult);
+}
+function gotResult(error,results) {
+    if (error) {
+        console.log(error);
+    }
+    console.log(results);
+}
